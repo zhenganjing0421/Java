58d3685713bf9d4fc5c80bb589f148c67c63b141
zhenganjing0421 <54836551+zhenganjing0421@users.noreply.github.com>
2019-12-29 21:47:33 +0800

    Create One_two_J_Circle.java

---
+++ One_two_J_Circle.java
@@ -0,0 +1,14 @@
+package one_two;
+
+public class J_Circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter,sum;
+		for(counter=1,sum=0;counter<=100;counter++)
+			sum+=counter;
+		System.out.println("counter="+counter+"，sum="+sum);
+
+	}
+
+}
