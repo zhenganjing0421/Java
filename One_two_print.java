4827386415db83aecb7434189a4d378176f67751
zhenganjing0421 <54836551+zhenganjing0421@users.noreply.github.com>
2019-12-29 21:40:23 +0800

    Create One_two_print.java

---
+++ One_two_print.java
@@ -0,0 +1,12 @@
+package one_two;
+
+public class print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Java语言，你好！");
+		System.out.print("我将成为优秀的Java程序员！");
+
+	}//方法main结束
+
+}//类print结束
